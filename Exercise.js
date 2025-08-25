05b33eb9ea73a909294bb5fbd049e912e27a6525
kurtramos-hub <kurt.ramos@hcdc.edu.ph>
2025-08-25 15:46:14 +0800

    Warm up Exercise

--- Exercise.js
+++ Exercise.js
@@ -4,9 +4,9 @@ let users = [
 ];
 
 let names = users.map(user => user.name);
-console.log(names); // ["Alice", "Bob"]
+console.log(names);
 
-// 2. Using .filter(), filter people with age 18 and above
+
 let people = [
   { name: "Tom", age: 16 },
   { name: "Jerry", age: 18 },
@@ -15,9 +15,7 @@ let people = [
 
 let adults = people.filter(person => person.age >= 18);
 console.log(adults); 
-// [{ name: "Jerry", age: 18 }, { name: "Spike", age: 22 }]
 
-// 3. Use reduce: Find the oldest person in the list
 let people2 = [
   { name: "Alice", age: 25 },
   { name: "Bob", age: 32 },
@@ -28,4 +26,4 @@ let oldestPerson = people2.reduce((oldest, person) => {
   return (person.age > oldest.age) ? person : oldest;
 });
 
-console.log(oldestPerson); // { name: "Bob", age: 32 }
+console.log(oldestPerson);
